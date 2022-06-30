0405ee69dd76b5d87e647e5cb665543cf8d8279f
Peter Chibunna <peterchibunna@gmail.com>
2022-06-30 16:28:08 -0700

    101 quotes #3

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,5 +10,6 @@ int main(void)
 	char quote[] = "and that piece of art is ";
 	char quote2[] = "useful\" - Dora Korpar, 2015-10-19\n";
 	fwrite(strcat(quote, quote2), 59, 1, stderr);
+
 	return (1);
 }
