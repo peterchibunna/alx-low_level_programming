53cf8323c65d0e41517298f0b4d44a389eb23f18
Peter Chibunna <peterchibunna@gmail.com>
2022-06-30 16:18:26 -0700

    kjsdfsdf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - " +
-	"Dora Korpar, 2015-10-19\n", 58, 1, stderr);
+	char *quote = "and that piece of art is " +
+		      "useful\" - Dora Korpar, 2015-10-19\n";
+	fwrite(ar, quote, 58, 1, stderr);
 	return (1);
 }
