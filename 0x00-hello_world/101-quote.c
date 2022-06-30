985e0fedab2868414372680d3b61deb434e6d773
Peter Chibunna <peterchibunna@gmail.com>
2022-06-30 16:04:56 -0700

    whsdfs

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 39, 1, stderr);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58, 1, stderr);
 	return (1);
 }
