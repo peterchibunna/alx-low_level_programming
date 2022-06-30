280ee9b1c4c671f50638ae2886926f0e1cec1f1d
Peter Chibunna <peterchibunna@gmail.com>
2022-06-30 16:25:04 -0700

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <string.h>
 /**
  * main - Entry point
  * Description: Print without fputs, printf, etc
@@ -7,8 +7,8 @@
  */
 int main(void)
 {
-	char *quote = "and that piece of art is " +
-		      "useful\" - Dora Korpar, 2015-10-19\n";
-	fwrite(quote, 58, 1, stderr);
+	char quote[] = "and that piece of art is ";
+	char quote2[]= "useful\" - Dora Korpar, 2015-10-19\n";
+	fwrite(strcat(quote, quote2), 58, 1, stderr);
 	return (1);
 }
