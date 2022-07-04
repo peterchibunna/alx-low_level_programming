b6be99044af607ddd328fde1e9316d3ba1f62282
Peter Chibunna <peterchibunna@gmail.com>
2022-07-04 15:35:06 -0700

    #10. possible bla bla bla

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,8 +16,9 @@ int main(void)
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
 
-			/* if (digit1 == 1 && digit2 == 0)*/
-			/*	continue;*/
+			/* stop at 89 since that's where uniqueness stops */
+			if (digit1 == 8 && digit2 == 9)
+				continue;
 
 			putchar(',');
 			putchar(' ');
