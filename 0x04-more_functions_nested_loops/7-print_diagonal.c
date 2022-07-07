a75774b03fe25cc3ad18308b71a0458b42d6759d
Peter Chibunna <peterchibunna@gmail.com>
2022-07-07 13:30:34 -0700

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal line that is `n`
+ *                  units long followed by new line character
+ * @n: Controls the size of the diagonal
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+	}
+	_putchar('\n');
+}
