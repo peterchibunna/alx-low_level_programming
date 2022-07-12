fec476d512a3375acd17bd74ba0a47603649c986
Peter Chibunna <peterchibunna@gmail.com>
2022-07-12 15:01:56 -0700

    rev..str...

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,6 +7,7 @@ void rev_string(char *s)
 {
 	int i = 0, j = 0;
 	int len = 0;
+	char s1, s2;
 
 	for (; s[i] != '\0'; i++)
 	{
@@ -15,8 +16,15 @@ void rev_string(char *s)
 	}
 	/*_putchar('\n');*/
 
+	i = 0;
 	for (j = len - 1; j >= 0; j--)
 	{
+		s1 = s[i];
+		s2 = s[j];
+
+		s[j] = s2;
+		s[i] = s1;
+		i += 1;
 		/*_putchar(s[j]);*/
 	}
 	/*_putchar('\n');*/
