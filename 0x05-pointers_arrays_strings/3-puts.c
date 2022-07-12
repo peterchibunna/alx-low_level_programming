1278db2b795ca4d7c87fab6870cec9340d85ad11
Peter Chibunna <peterchibunna@gmail.com>
2022-07-12 14:27:18 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - code definition
+ * @str: The string parameter
+ */
+void _puts(char *str)
+{
+	int i = 0;
+	int len = 0;
+
+	for (; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
