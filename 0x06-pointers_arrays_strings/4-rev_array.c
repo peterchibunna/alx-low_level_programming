f1f74912d80358769acde82dcdfd062236e7563d
Peter Chibunna <peterchibunna@gmail.com>
2022-07-15 11:13:26 -0700

    reverse array...

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,13 +10,13 @@ void reverse_array(int *a, int n)
 	int i, j;
 	int current_element;
 
-	j = --n; /*decrement n and assign to j */
-	while (i < n / 2)
+	j = --n;
+
+	for (i = 0; i < n / 2; i++)
 	{
 		current_element = a[i];
 		a[i] = a[j];
 		a[j] = current_element;
 		j--;
-		i++;
 	}
 }
