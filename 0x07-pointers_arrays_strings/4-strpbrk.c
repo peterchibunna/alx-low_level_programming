3bc571e12fe99d2219e5718eed34f5c627b39c7a
Peter Chibunna <peterchibunna@gmail.com>
2022-07-18 13:18:05 -0700

    #4 task

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,11 +9,11 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	int i = 0, j = 0;
+	int i, j;
 
-	for (; s[i]; i++)
+	for (i = 0; s[i]; i++)
 	{
-		for (; accept[j]; j++)
+		for (j = 0; accept[j]; j++)
 		{
 			if (accept[j] == s[i])
 			{
