fcea8b595f30f4e4dac0c05a2bc682f7dbc18bb8
Peter Chibunna <peterchibunna@gmail.com>
2022-08-01 11:36:12 -0700

    task #2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * print_dog - prints a `dog`
+ * @d: the dog object
+ **/
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+		{
+			printf("Name: %s\n", d->name);
+		}
+		else
+		{
+			printf("Name: (nil)\n");
+		}
+
+		if (d->age)
+		{
+			printf("Age: %f\n", d->age);
+		}
+		else
+		{
+			printf("Age: (nil)\n");
+		}
+		if (d->owner != NULL)
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+		else
+		{
+			printf("Owner: (nil)\n");
+		}
+	}
+}
