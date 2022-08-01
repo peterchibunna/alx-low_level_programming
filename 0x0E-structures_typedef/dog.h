2dd61d04c2b65d10a76eade30eae25e287e95930
Peter Chibunna <peterchibunna@gmail.com>
2022-08-01 12:17:26 -0700

    task #4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,5 +18,8 @@ typedef struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
 
 #endif
