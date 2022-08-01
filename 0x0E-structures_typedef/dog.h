72e3afba92b3b5393f012b84a1defa58c92b0c33
Peter Chibunna <peterchibunna@gmail.com>
2022-08-01 10:52:36 -0700

    task #0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Defines a dog structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: Owner of dog
+ **/
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} description;
+
+
+#endif
