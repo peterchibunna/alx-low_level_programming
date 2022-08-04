210fe58a1cb0b22c2536d88143227cd6c4c6cdbb
Peter Chibunna <peterchibunna@gmail.com>
2022-08-04 13:09:23 -0700

    task #3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,6 +7,15 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
+/**
+ * struct format - out based on the formatting character
+ * @formatter: format character
+ * @method: Function pointer to the corresponding type function
+ **/
+typedef struct format
+{
+	char formatter;
+	void (*method)(va_list);
+} format_t;
 
 #endif
