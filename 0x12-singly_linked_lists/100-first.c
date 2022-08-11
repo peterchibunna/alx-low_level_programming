511c12a260fcf738d9716f3199e84bf07f2f591a
Peter Chibunna <peterchibunna@gmail.com>
2022-08-11 14:22:07 -0700

    task #100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print - Prints before main is executed
+ **/
+void exec(void) __attribute__ ((constructor));
+void exec(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
