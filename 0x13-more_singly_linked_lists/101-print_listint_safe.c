db48d493dcaa87732e5492272144aeeb8b4955fb
Peter Chibunna <peterchibunna@gmail.com>
2022-08-16 13:46:02 -0700

    task #101

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * print_listint_safe - print a listint_t linked list
+ * @head: head of the list
+ *
+ * Return: Numbers of elements of the list
+ **/
+size_t print_listint_safe(const listint_t *head)
+{
+	int i;
+
+	if (head == NULL)
+		return (0);
+
+	for (i = 0; head != NULL; i++)
+	{
+		printf("[%p] %d\n", (void *) head, head->n);
+
+		if (head <= head->next)
+		{
+			head = head->next;
+			printf("-> [%p] %d\n", (void *) head, head->n);
+			i++;
+			return (i);
+		}
+
+		head = head->next;
+	}
+	return (i);
+}
