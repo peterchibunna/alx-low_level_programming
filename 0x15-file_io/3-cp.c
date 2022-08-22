d4bd5d5a0990a9b760932c83dc4994a9dd386d63
Peter Chibunna <peterchibunna@gmail.com>
2022-08-22 12:57:46 -0700

    task #3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -33,7 +33,7 @@ void print_error(char *msg, char *extra, int status)
 }
 
 /**
- * main - copies the content of a file to another file.
+ * main -[?12;4$y copies the content of a file to another file.
  * @argc: arguments count
  * @argv: Argument vector
  *
@@ -41,41 +41,41 @@ void print_error(char *msg, char *extra, int status)
  **/
 int main(int argc, char **argv)
 {
-	int fd_from, fd_to, b_readed, b_writed;
+	int fd_source, fd_dest, bytes_read, bytes_written;
 	int buffer_size = 1024;
 	char buff[1024];
 
 	if (argc != 3)
 		print_error("Usage: cp file_from file_to", "", 97);
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
+	fd_source = open(argv[1], O_RDONLY);
+	if (fd_source == -1)
 		print_error("Error: Can't read from file ", argv[1], 98);
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-		print_and_close_fd("Error: Can't write to ", argv[2], 99, fd_from);
+	fd_dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_dest == -1)
+		print_and_close_fd("Error: Can't write to ", argv[2], 99, fd_source);
 
-	b_readed = read(fd_from, buff, buffer_size);
-	while (b_readed > 0)
+	bytes_read = read(fd_source, buff, buffer_size);
+	while (bytes_read > 0)
 	{
-		b_writed = write(fd_to, buff, b_readed);
-		if (b_readed != b_writed)
-			print_and_close_fd("Error: Can't write to ", argv[2], 99, fd_from);
+		bytes_written = write(fd_dest, buff, bytes_read);
+		if (bytes_read != bytes_written)
+			print_and_close_fd("Error: Can't write to ", argv[2], 99, fd_source);
 
-		b_readed = read(fd_from, buff, buffer_size);
+		bytes_read = read(fd_source, buff, buffer_size);
 	}
 
-	if (b_readed == -1)
+	if (bytes_read == -1)
 		print_error("Error: Can't read from file ", argv[1], 98);
 
-	b_readed = close(fd_from);
-	if (b_readed < 0)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+	bytes_read = close(fd_source);
+	if (bytes_read < 0)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_source), exit(100);
 
-	b_writed = close(fd_to);
-	if (b_writed < 0)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+	bytes_written = close(fd_dest);
+	if (bytes_written < 0)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_dest), exit(100);
 
 	return (0);
 }
