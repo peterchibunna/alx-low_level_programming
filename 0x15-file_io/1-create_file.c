7bbe7edf24b139f28d517f5f75be0c296513bfbe
Peter Chibunna <peterchibunna@gmail.com>
2022-08-22 11:52:44 -0700

    task #1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ **/
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i = 0, result;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		return (1);
+
+	while (text_content[i] != 0)
+	{
+		i++;
+	}
+
+	result = write(fd, text_content, i);
+	close(fd);
+
+	if (result == -1)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (1);
+	}
+}
+
