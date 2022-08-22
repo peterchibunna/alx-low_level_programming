ad3731bcf79a960e04927b60ce6409eddb16037f
Peter Chibunna <peterchibunna@gmail.com>
2022-08-22 12:55:26 -0700

    task #3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,37 @@
 #include "main.h"
-#include "3-goto_error.c"
+
+void print_and_close_fd(char *msg, char *extra, int status, int fd);
+void print_error(char *msg, char *extra, int status);
+
+
+/**
+ * print_and_close_fd - Print the error and close the file descriptor
+ * @msg: message to show
+ * @extra: Extra optional arguments
+ * @status: Exit status code
+ * @fd: File descriptor
+ **/
+void print_and_close_fd(char *msg, char *extra, int status, int fd)
+{
+	if (fd > 0)
+	{
+		close(fd);
+	}
+	print_error(msg, extra, status);
+}
+
+/**
+ * print_error - Print the error and close the file descriptor
+ * @msg: message to show
+ * @extra: Extra arguments
+ * @status: Exit status code
+ **/
+void print_error(char *msg, char *extra, int status)
+{
+	dprintf(STDERR_FILENO, "%s%s\n", msg, extra);
+	exit(status);
+}
+
 /**
  * main - copies the content of a file to another file.
  * @argc: arguments count
