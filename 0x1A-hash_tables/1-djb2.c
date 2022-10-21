51087a635551d389db2ac49f4181a19cfe08588a
Peter Chibunna <peterchibunna@gmail.com>
2022-10-21 14:23:49 +0100

    Task #1

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+/*source: https://gist.github.com/papamuziko/7bb52dfbb859fdffc4bd0f95b76f71e8*/
+
+/**
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
+}
--- 0x1A-hash_tables/README.md
+++ 0x1A-hash_tables/README.md
@@ -1,1 +1,8 @@
 ## 0x1A-hash_tables
+
+1. `gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-hash_table_create.c -o a`
+<br>Test for memory leaks with `valgrind ./a`
+
+2. `gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-djb2.c -o b`
+
+3.
